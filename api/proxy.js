adc35e0617823cd9996e0cff53c91235769c4f4a
Joseph <looooooooooooooooooooooooooooooooooooooooooooool@hotmail.com>
2026-02-02 13:07:47 -0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     // Visit your GitHub profile page
-    const targetUrl = "https://camo.githubusercontent.com/66601927b54aa5fe2504158f0f481cbac0e4e288f52ea4a87d29ee10cf09f0f5/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d6a336f65";
+    const targetUrl = "https://krunker.io";
 
     const response = await fetch(targetUrl, {
       headers: {
