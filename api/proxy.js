2227b37df1f23504cd3f21199a74baf13acc0260
Joseph <looooooooooooooooooooooooooooooooooooooooooooool@hotmail.com>
2026-02-19 19:30:53 -0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     // Visit your GitHub profile page
-    const targetUrl = "https://github.com/loooooooser";
+    const targetUrl = "https://t.me/s/czceh";
 
     const response = await fetch(targetUrl, {
       headers: {
